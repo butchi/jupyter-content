3724f9ca3e16690a2ef3f66a115fe90512350073
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2024-03-31 10:18:32 +0900

    render

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,16 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc>
+      <template #default="{ doc }">
+        <article>
+          <ContentRenderer :value="doc"></ContentRenderer>
+        </article>
+      </template>
+      <template #not-found>
+        <p>
+            コンテンツが見つかりません
+        </p>
+      </template>
+    </ContentDoc>
   </main>
 </template>
