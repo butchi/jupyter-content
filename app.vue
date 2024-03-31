b9f61862b0ea58752b32bb026dc2b179120f172e
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2024-03-31 10:19:21 +0900

    スタイル定義

--- app.vue
+++ app.vue
@@ -3,3 +3,21 @@
     <NuxtPage />
   </div>
 </template>
+
+<style lang="postcss">
+.source.code pre {
+  padding: 1em;
+
+  border: 1px solid hsl(0, 0%, 85%);
+
+  background-color: hsl(0, 0%, 95%);
+
+  border-radius: 3px;
+}
+
+.text.html {
+  + .text.plain {
+    display: none;
+  }
+}
+</style>
