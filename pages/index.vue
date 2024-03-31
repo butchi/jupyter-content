733e0188d4ed2b8801979fb5f0f5434c950979a2
IWABUCHI Yu(u)ki <butchiyu+com-github@gmail.com>
2024-03-31 10:19:00 +0900

    index

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <ContentList path="/" v-slot="{ list }">
+      <div v-for="article in list" :key="article._path">
+        <h2><nuxt-link :to="article._path + '/'">{{ article.title }}</nuxt-link></h2>
+        <p>{{ article.description }}</p>
+        <!-- <ContentRenderer :value="article" class="doc"></ContentRenderer> -->
+      </div>
+    </ContentList>
+  </main>
+</template>
